f31b2a009e1efc947d2141f708c7fd66bf5d98b8
Amit Grover <amitg90@gmail.com>
2018-01-12 16:15:24 -0800

    Made the data more meaning ful based on solution provided

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -33,7 +33,17 @@ public class MainActivity extends AppCompatActivity {
         weather_view = (TextView) findViewById(R.id.tv_weather_data);
 
         // TODO (3) Create an array of Strings that contain fake weather data
-        String[] fake_data = {"Sunny", "Rainy", "Hot", "Cold", "Freezing", "boiling hot", "partly cloudy"};
+        String[] fake_data = {"Today, Jan 12 - Sunny - 17°C / 15°C",
+                              "Yesterday, Jan 11 - Rainy - 47°C / 35°C",
+                              "Jan 10 - HOT - 80°C / 65°C",
+                              "Jan 9 - Freezing - 0°C / 0°C",
+                              "Jan 8 - HOT - 80°C / 65°C",
+                              "Jan 7 - Freezing - 0°C / 0°C",
+                              "Jan 6 - HOT - 80°C / 65°C",
+                              "Jan 5 - Partly Cloudy - 34°C / 32°C",
+                              "Jan 4 - HOT - 45°C / 54°C",
+                              "Jan 3 - Rainy - 23°C / 32°C",
+        };
 
         // TODO (4) Append each String from the fake weather data array to the TextView
         for (String str : fake_data) {

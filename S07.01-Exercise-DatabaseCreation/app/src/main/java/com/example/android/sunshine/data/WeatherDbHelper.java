1115928e1e9e9dd94c3efb643f3f86ebbc8c91d6
Amit Grover <amitg90@gmail.com>
2018-01-27 23:14:27 -0800

    S07.01-Exercise-DatabaseCreation completed

--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -15,11 +15,45 @@
  */
 package com.example.android.sunshine.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 /**
  * Manages a local database for weather data.
  */
 // TODO (11) Extend SQLiteOpenHelper from WeatherDbHelper
-public class WeatherDbHelper {
+public class WeatherDbHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "weather.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public WeatherDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        Log.e("!!AMIT!!", "Creating Table");
+        final String SQL_CREATE_SUNSHINE_TABLE = "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + " (" +
+                WeatherContract.WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                WeatherContract.WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_DEGREES + " TEXT NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_HUMIDITY + " INTEGER NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_MAX_TEMP + " INTEGER NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_MIN_TEMP + " INTEGER NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_PRESSURE + " INTEGER NOT NULL, " +
+                WeatherContract.WeatherEntry.COLUMN_DATE + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+                WeatherContract.WeatherEntry.COLUMN_WIND_SPEED + " INTEGER NOT NULL " +
+                "); ";
+        Log.e("AMIT!!", SQL_CREATE_SUNSHINE_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_SUNSHINE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 
 //  TODO (12) Create a public static final String called DATABASE_NAME with value "weather.db"
 
